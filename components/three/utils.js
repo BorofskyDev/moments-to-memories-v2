0b3d20367fdeba98ad4275c843cae1c71e07e840
solovyovsdom <joelborofsky@mac.com>
2024-09-26 21:59:26 -0500

    updated responsiveness

--- components/three/utils.js
+++ components/three/utils.js
@@ -5,3 +5,15 @@ export const getPiramidalIndex = (array, index) =>
   array.map((_, i) =>
     index === i ? array.length : array.length - Math.abs(index - i)
   )
+
+
+
+export const getImageDimensions = (src) => {
+  return new Promise((resolve) => {
+    const img = new Image()
+    img.onload = () => {
+      resolve({ width: img.width, height: img.height })
+    }
+    img.src = src
+  })
+}
