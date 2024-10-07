b02294bbd3df1bc1be6f5a184574aaf0636aab7f
solovyovsdom <joelborofsky@mac.com>
2024-10-07 12:08:17 -0500

    updated mobile styling for contact form

---
+++ components/layout/input-field/InputField.jsx
@@ -0,0 +1,67 @@
+// components/layout/input-field/InputField.jsx
+
+'use client'
+
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './InputField.module.scss'
+import BodyText from '@/components/layout/body-text/BodyText'
+
+const InputField = ({
+  label,
+  name,
+  type = 'text',
+  value,
+  onChange,
+  required = false,
+  error = '',
+  as = 'input', // 'input' or 'textarea'
+  ...rest
+}) => {
+  return (
+    <div className={styles.formGroup}>
+      <label htmlFor={name}>
+        {label}
+        {required && <span className={styles.required}>*</span>}
+      </label>
+
+      {as === 'textarea' ? (
+        <textarea
+          id={name}
+          name={name}
+          value={value}
+          onChange={onChange}
+          className={`${styles.inputField} ${error ? styles.errorInput : ''}`}
+          required={required}
+          {...rest}
+        ></textarea>
+      ) : (
+        <input
+          type={type}
+          id={name}
+          name={name}
+          value={value}
+          onChange={onChange}
+          className={`${styles.inputField} ${error ? styles.errorInput : ''}`}
+          required={required}
+          {...rest}
+        />
+      )}
+
+      {error && <BodyText className={styles.errorText}>{error}</BodyText>}
+    </div>
+  )
+}
+
+InputField.propTypes = {
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  required: PropTypes.bool,
+  error: PropTypes.string,
+  as: PropTypes.oneOf(['input', 'textarea']),
+}
+
+export default InputField
--- components/pages/home/contact-section/contact-form/ContactForm.jsx
+++ components/pages/home/contact-section/contact-form/ContactForm.jsx
@@ -6,8 +6,9 @@ import React from 'react'
 import useContactForm from '@/libs/hooks/contact-form/useContactForm'
 import styles from './ContactForm.module.scss'
 import BodyText from '@/components/layout/body-text/BodyText'
-import MediumBodyText from '@/components/layout/body-text/medium-body-text/MediumBodyText'
 import SectionHeading from '@/components/headings/section-heading/SectionHeading'
+import InputField from '@/components/layout/input-field/InputField' // Import the InputField component
+import SubmitButton from '@/components/buttons/submit-button/SubmitButton'
 
 const ContactForm = () => {
   const {
@@ -22,8 +23,6 @@ const ContactForm = () => {
 
   return (
     <form className={styles.contactForm} onSubmit={handleSubmit}>
-      <SectionHeading>Contact Us</SectionHeading>
-
       {success && (
         <BodyText className={styles.successMessage}>
           Your message has been sent successfully!
@@ -35,89 +34,53 @@ const ContactForm = () => {
       )}
 
       {/* Name Field */}
-      <div className={styles.formGroup}>
-        <label htmlFor='name'>
-          Name<span className={styles.required}>*</span>
-        </label>
-        <input
-          type='text'
-          id='name'
-          name='name'
-          value={formData.name}
-          onChange={handleChange}
-          className={`${styles.inputField} ${
-            errors.name ? styles.errorInput : ''
-          }`}
-          required
-        />
-        {errors.name && (
-          <BodyText className={styles.errorText}>{errors.name}</BodyText>
-        )}
-      </div>
+      <InputField
+        label='Name'
+        name='name'
+        value={formData.name}
+        onChange={handleChange}
+        required
+        error={errors.name}
+      />
 
       {/* Email Field */}
-      <div className={styles.formGroup}>
-        <label htmlFor='email'>
-          Email<span className={styles.required}>*</span>
-        </label>
-        <input
-          type='email'
-          id='email'
-          name='email'
-          value={formData.email}
-          onChange={handleChange}
-          className={`${styles.inputField} ${
-            errors.email ? styles.errorInput : ''
-          }`}
-          required
-        />
-        {errors.email && (
-          <BodyText className={styles.errorText}>{errors.email}</BodyText>
-        )}
-      </div>
+      <InputField
+        label='Email'
+        name='email'
+        type='email'
+        value={formData.email}
+        onChange={handleChange}
+        required
+        error={errors.email}
+      />
 
       {/* Phone Number Field */}
-      <div className={styles.formGroup}>
-        <label htmlFor='phone'>
-          Phone Number<span className={styles.required}>*</span>
-        </label>
-        <input
-          type='tel'
-          id='phone'
-          name='phone'
-          value={formData.phone}
-          onChange={handleChange}
-          className={`${styles.inputField} ${
-            errors.phone ? styles.errorInput : ''
-          }`}
-          required
-        />
-        {errors.phone && (
-          <BodyText className={styles.errorText}>{errors.phone}</BodyText>
-        )}
-      </div>
+      <InputField
+        label='Phone Number'
+        name='phone'
+        type='tel'
+        value={formData.phone}
+        onChange={handleChange}
+        required
+        error={errors.phone}
+        // Uncomment the following lines if using react-input-mask
+        // as="input"
+        // inputComponent={InputMask}
+        // mask="999-999-9999"
+        // maskChar=""
+      />
 
       {/* Subject Field */}
-      <div className={styles.formGroup}>
-        <label htmlFor='subject'>
-          Subject<span className={styles.required}>*</span>
-        </label>
-        <input
-          type='text'
-          id='subject'
-          name='subject'
-          value={formData.subject}
-          onChange={handleChange}
-          className={`${styles.inputField} ${
-            errors.subject ? styles.errorInput : ''
-          }`}
-          maxLength={100}
-          required
-        />
-        {errors.subject && (
-          <BodyText className={styles.errorText}>{errors.subject}</BodyText>
-        )}
-      </div>
+      <InputField
+        label='Subject'
+        name='subject'
+        type='text'
+        value={formData.subject}
+        onChange={handleChange}
+        required
+        error={errors.subject}
+        maxLength={100}
+      />
 
       {/* Preferred Contact Method Field */}
       <div className={styles.formGroup}>
@@ -146,30 +109,24 @@ const ContactForm = () => {
       </div>
 
       {/* Message Field */}
-      <div className={styles.formGroup}>
-        <label htmlFor='message'>
-          Message<span className={styles.required}>*</span>
-        </label>
-        <textarea
-          id='message'
-          name='message'
-          value={formData.message}
-          onChange={handleChange}
-          className={`${styles.textareaField} ${
-            errors.message ? styles.errorInput : ''
-          }`}
-          maxLength={500}
-          required
-        ></textarea>
-        {errors.message && (
-          <BodyText className={styles.errorText}>{errors.message}</BodyText>
-        )}
-      </div>
+      <InputField
+        label='Message'
+        name='message'
+        as='textarea'
+        value={formData.message}
+        onChange={handleChange}
+        required
+        error={errors.message}
+        maxLength={500}
+      />
 
       {/* Submit Button */}
-      <button type='submit' className={styles.submitButton} disabled={loading}>
-        {loading ? 'Sending...' : 'Send Message'}
-      </button>
+      <SubmitButton
+        type='submit'
+        className={styles.submitButton}
+        disabled={loading}
+        text={loading ? 'Submitting...' : 'Submit Message'}
+      />
     </form>
   )
 }
